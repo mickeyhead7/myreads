04c505e9970c4130213392ef675ccc04730de097
Mark Head <mickeyhead7@gmail.com>
2017-08-07 00:14:16 +0100

    feature: Corrected links
    Corrected links, utilising Link from react-router-dom, to maintain state when switching views

--- src/BookList.js
+++ src/BookList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Bookshelf from './Bookshelf';
 
 class BookList extends Component {
@@ -28,7 +29,7 @@ class BookList extends Component {
                     </div>
                 </div>
                 <div className="open-search">
-                    <a href="/search">Add a book</a>
+                    <Link to="/search">Add a book</Link>
                 </div>
             </div>
         );
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import DebounceInput from 'react-debounce-input';
 import Book from './Book';
 
@@ -15,7 +16,7 @@ class SearchPage extends Component {
         return (
             <div className="search-books">
                 <div className="search-books-bar">
-                    <a href="/" className="close-search">Close</a>
+                    <Link to="/" className="close-search">Close</Link>
                     <div className="search-books-input-wrapper">
                         <DebounceInput
                             type="text"
